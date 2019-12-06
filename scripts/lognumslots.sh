f3e3991677ef96cfbf8c5d590b02cd3f9dbae25c
Prashant Pandey <prashant.prashn@gmail.com>
2019-12-06 13:01:37 -0800

    Update lognumslots.sh script according to the new ntCard format.

--- scripts/lognumslots.sh
+++ scripts/lognumslots.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# This script takes as input the path to the output file of 'ntCard'. It then
-# calculates log of the number of slots needed by Squeakr to count k-mers.
+# This script takes two input arguments:
+# 1. The path to the output file of 'ntCard'
+# 2. F0 value output by ntCard. 
+# It then calculates log of the number of slots needed by Squeakr to count
+# k-mers.
 
-F0=`grep -P "^F0\t" $1 | cut -f2- -d$'\t'`
-f1=`grep -P "^1\t" $1 | cut -f2- -d$'\t'`
-f2=`grep -P "^2\t" $1 | cut -f2- -d$'\t'`
+F0=$2
+f1=`grep -P "\t1\t" $1 | cut -f3- -d$'\t'`
+f2=`grep -P "\t2\t" $1 | cut -f3- -d$'\t'`
 fgt2=$[ $F0 - $f1 - $f2 ]
 ns=$[ $f1 + 2 * $f2 + 3 * $fgt2 ]
 echo "x=$ns;l2=l(x)/l(2);s=scale;scale=0;l2ru=l2+1-(l2%1);np2=2^(l2ru/1);scale=s;if(x > (0.9*np2)) { l2ru=l2ru+1; np2=2*np2; }; s=scale; scale=0; print l2ru/1; scale=s;" | bc -l
